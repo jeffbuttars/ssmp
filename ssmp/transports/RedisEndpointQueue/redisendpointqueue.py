92dfd0dff45d8cace613ed698ee09169a410aa02
Jeff Buttars <jeffbuttars@gmail.com>
2013-04-30 19:16:55 -0600

    Tweaking endpoint tansport

--- ssmp/transports/RedisEndpointQueue/redisendpointqueue.py
+++ ssmp/transports/RedisEndpointQueue/redisendpointqueue.py
@@ -1,9 +1,28 @@
+import logging
+logger = logging.getLogger("ssmp")
+
 from ssmp.transports import RedisBasicQueue
 
 
 class RedisEndpointQueue(RedisBasicQueue):
-    def __init__(self, msg_class='endpoint', **kwargs):
+    def __init__(self, **kwargs):
         """todo: to be defined """
-        super(kwargs, self).__init__(msg_class, **kwargs)
+        kwargs = kwargs or {}
+        kwargs['msg_class'] = 'endpoint'
+        super(kwargs, self).__init__(**kwargs)
     #__init__()
+
+    def push(self, ep, payload, q=None):
+        """todo: Docstring for send
+        """
+
+        logger.debug("ep: %s, payload: %s, q:%s", ep, payload, q)
+
+        logger.debug(self._msg_cls)
+        m = self._msg_cls(ep, payload)
+
+        rq = q or self._default_q
+        return self._conn.lpush(rq, m.msg) and m
+    #push()
+
 #RedisEndpointQueue
